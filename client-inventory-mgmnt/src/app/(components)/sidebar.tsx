750c111f6a5698cf0aecca3d81c27d69447da9f2
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-11-10 20:55:54 +0300

    implement sidebar toggle functionality and update navbar and sidebar components

--- client-inventory-mgmnt/src/app/(components)/sidebar.tsx
+++ client-inventory-mgmnt/src/app/(components)/sidebar.tsx
@@ -1,18 +1,68 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Icon, LucideIcon, Menu } from "lucide-react";
 import React from "react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinkProps) => {
+
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return(
+    <Link></Link>
+  )
+
+
+};
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } shadow-md z-40 h-full bg-white transition-all duration-300 overflow-hidden`;
+
   return (
-    <div>
+    <div className={sidebarClassNames}>
       {/* Sidebar Logo */}
-      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8 ">
+      <div
+        className={`flex gap-3 justify-between md:justify-normal items-center 
+         pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}
+      >
         <div>Logo</div>
-        <h1 className="font-extrabold text-2xl">Farm Produce</h1>
+        <h1
+          className={`font-extrabold text-2xl ${
+            isSidebarCollapsed ? "hidden" : "block"
+          }`}
+        >
+          Farm Produce
+        </h1>
         <button
-          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100
-            onClick={()=> {}} "
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
